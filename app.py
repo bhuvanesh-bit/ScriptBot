1892104a8169960563ece88e43f20d5eeb58fd18
bhuvanesh-bit <Bhuvanesh450kumar@gmail.com>
2025-05-19 16:00:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -266,7 +266,7 @@ with st.form("input_form", clear_on_submit=True):
     with col1:
         question = st.text_area("Ask a coding question", label_visibility="collapsed", height=80)
     with col2:
-        submit = st.form_submit_button("⇧")
+        submit = st.form_submit_button("https://techknowprime.com/wp-content/uploads/2022/12/Chonky-Submit-Button.webp")
     if submit and question:
         try:
             answer = generate_code(question)
